4e73cc9a46510ae17ca61a0e6763ebf6bfa7e3ee
Ali Adnan <Ali Adnan>
2017-01-18 00:16:26 +0500

    Teacher's side Api done

--- ECourses/Controllers/TeachersApiController.cs
+++ ECourses/Controllers/TeachersApiController.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Web.Http;
 using ECourses.Models;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace ECourses.Controllers
 {
@@ -85,11 +86,20 @@ namespace ECourses.Controllers
 
         public bool InsertCourse(Course obj)
         {
+            if (obj.Id == 0)
+            {
 
+                dbef.Courses.Add(obj);
+                
+            }
+            else
+            {
+                Course temp = dbef.Courses.Where(x => x.Id == obj.Id).FirstOrDefault();
+                temp = obj;
+                
 
-            dbef.Courses.Add(obj);
+            }
             dbef.SaveChanges();
-
 
             return true;
 
@@ -98,6 +108,63 @@ namespace ECourses.Controllers
 
 
         }
+
+
+
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/TeachersApiController/LoginPost")]
+
+        public ActionResult LoginPost(HttpRequestMessage value)
+
+        {
+            string v = value.Content.ReadAsStringAsync().Result;
+            Teacher obj= JsonConvert.DeserializeObject<Teacher>(v);
+            string name = obj.User_Name;
+            string pass = obj.Password;
+            Teacher obj1 = dbef.Teachers.Where(x => x.User_Name.Equals(name) && x.Password.Equals(pass)).FirstOrDefault();
+
+            var list = obj1.Courses.Select(s=> new { s.Id,s.Title,s.Teacher.First_Name });
+
+
+            return new JsonResult() { Data = list, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+          
+        }
+
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/TeachersApiController/GetCourseDetails")]
+
+        public ActionResult GetCourseDetails(HttpRequestMessage value)
+
+        {
+            string v = value.Content.ReadAsStringAsync().Result;
+            Course obj = JsonConvert.DeserializeObject<Course>(v);
+            var ret = dbef.Courses.Where(x => x.Id == obj.Id || x.Title.Equals(obj.Title)).Select(s=> new { s.Id,s.Title,s.Address,s.Course_Type,s.Description,s.Created_At,s.Duration,s.Image,s.Likes,s.Views,s.Updated_At,s.Start_Date,s.End_Date}).FirstOrDefault();
+           
+
+
+            return new JsonResult() { Data = ret, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
+        }
+
+
+
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/TeachersApiController/getAllTeachers")]
+
+        public ActionResult getAllTeachers()
+
+        {
+
+            List<Teacher> obj=dbef.Teachers.Where(x=>x.Id!=0).ToList();
+
+
+            return new JsonResult() { Data = obj, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+
+        }
+
 
 
         [System.Web.Http.HttpGet]
